a90d9d4b9ae2092ef0e244362db1106ae6700a68
mike <michaeldotson@protonmail.com>
2019-03-13 11:07:23 -0700

    adds RESTful routing for Student model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace  :api do
     get "/students" => "students#index"
-
-
+    get "/students/:id" => "students#show"
+    post "/students" => "students#create"
+    patch "/students/:id" => "students#update"
+    delete "/students/:id" => "students#destroy"
   end
 
 
